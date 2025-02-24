650cef932bbc122655a154a1c9d4526a62936ed6
Ryan Deist <ryandeist@gmail.com>
2025-02-24 16:29:12 -0500

    Cat Collector - Base Template and About Template created.

--- main_app/views.py
+++ main_app/views.py
@@ -7,4 +7,4 @@ def home(request):
     return HttpResponse('<h1>Hello Cat</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About the CatCollector</h1>')
+    return render(request, 'about.html')
