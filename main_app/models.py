39a075db31ac8d350ce510629d849c7ab1648be9
Ryan Deist <ryandeist@gmail.com>
2025-02-26 18:22:13 -0500

    Feeding model added.

--- main_app/models.py
+++ main_app/models.py
@@ -14,3 +14,7 @@ class Cat(models.Model):
     def get_absolute_url(self):
         return reverse("cat-detail", kwargs={"cat_id": self.id})
     
+class Feeding(models.Model):
+    date = models.DataField()
+    meal = models.CharField(max_length=1)
+    
