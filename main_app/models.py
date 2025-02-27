53ca0616976dace11693b06ee2b65c63fbf603b9
Ryan Deist <ryandeist@gmail.com>
2025-02-26 19:17:29 -0500

    Connected Feeding Model to Cat Model, updated UI to display feedings on cat detail page.

--- main_app/models.py
+++ main_app/models.py
@@ -21,9 +21,14 @@ class Cat(models.Model):
         return reverse("cat-detail", kwargs={"cat_id": self.id})
     
 class Feeding(models.Model):
-    date = models.DataField()
+    date = models.DateField('Feeding Date')
     meal = models.CharField(
         max_length=1,
         choices=MEALS,
         default=MEALS[0][0]
     )
+    
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}."
