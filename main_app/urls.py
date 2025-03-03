2ac6fcf78057d2825342d662f7020874b5e386ab
Ryan Deist <ryandeist@gmail.com>
2025-03-03 13:05:28 -0500

    Toy Index Template built and styled.

--- main_app/urls.py
+++ main_app/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
         name='add-feeding'
     ),
     path('toys/create/', views.ToyCreate.as_view(), name='toy-create'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toy-detail'),
+    path('toys/', views.ToyList.as_view(), name='toy-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .models import Cat, Toy
 from .forms import FeedingForm
 
@@ -44,3 +45,9 @@ class CatDelete(DeleteView):
 class ToyCreate(CreateView):
     model = Toy
     fields = '__all__'
+
+class ToyList(ListView):
+    model = Toy
+    
+class ToyDetail(DetailView):
+    model = Toy
