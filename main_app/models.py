3dde43fe69f6da301d7142ddf412a88462d1a739
Ryan Deist <ryandeist@gmail.com>
2025-03-05 16:10:49 -0500

    built custom method to show message if cat might be hungry on Add Feeding widget.

--- main_app/models.py
+++ main_app/models.py
@@ -34,6 +34,9 @@ class Cat(models.Model):
     def get_absolute_url(self):
         return reverse("cat-detail", kwargs={"cat_id": self.id})
     
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+    
 class Feeding(models.Model):
     date = models.DateField('Feeding Date')
     meal = models.CharField(
